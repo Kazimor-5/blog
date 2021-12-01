7f0f32f902017241b950ccec874b673030389d7a
Alex <alexben@live.fr>
2021-12-01 13:33:21 +0100

    ajout relation page serveur

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -6,6 +6,7 @@ use App\Entity\Article;
 use App\Form\ArticleType;
 use DateTime;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -23,11 +24,48 @@ class BlogController extends AbstractController
     /** 
     * @Route("/edit/{id}", name= "article_edit")
     */
-    public function edit($id): Response
-    {
-        return $this->render('blog/edit.html.twig', [
-            "id" => $id
-        ]);
+    public function edit(Article $article, Request $request): Response {
+        $oldPicture = $article->getPicture();
+
+        $form = $this->createForm(ArticleType::class, $article);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $article->setLastUpdateTime(new \DateTime());
+
+            if ($article->getIsPublished()) {
+                $article->setPublicationDate(new \DateTime());
+            }
+
+            if ($article->getPicture() !== null && $article->getPicture() !== $oldPicture) {
+                $file = $form->get('picture')->getData();
+                $fileName = uniqid() . '.' . $file->guessExtension();
+
+            try {
+                $file->move(
+                $this->getParameter('images_directory'),
+                $fileName
+                );
+            } catch (FileException $e) {
+                return new Response($e->getMessage());
+            }
+
+            $article->setPicture($fileName);
+            } else {
+                $article->setPicture($oldPicture);
+            }
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($article);
+            $em->flush();
+
+            return new Response('L\'article a bien été modifié.');
+        }
+
+    return $this->render("blog/edit.html.twig", [
+        "article" => $article,
+        "form" => $form->createView()
+    ]);
     }
 
     /** 
@@ -41,6 +79,24 @@ class BlogController extends AbstractController
         $form->handleRequest(($request));
         if($form->isSubmitted() && $form->isValid()) {
             $article->setLastUpdateTime(new DateTime());
+
+            if ($article->getPicture() != null) {
+                $file = $form->get("picture")->getData();
+                $filename = uniqid() . "." . $file->guessExtension();
+
+                try {
+                    $file->move(
+                // Le dossier dans lequel le fichier va être chargé
+                    $this->getParameter("images_directory"),
+                    $filename
+                );
+                } catch (FileException $e) {
+                    return new Response($e->getMessage());
+                }
+
+                $article->setPicture($filename);
+            }
+
             if($article->getIsPublished()) {
                 $article->setPublicationDate(new DateTime());
             }
